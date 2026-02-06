ab40bf9d5dc096199632b2a48eb4036a460e4e8d
Miroux Julien <julien.miroux.etu@b13p14.fil.univ-lille.fr>
2026-02-06 08:47:08 +0100

    supp tp1

--- tp1/src/main/java/com/example/tp1/data/CommandeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tp1.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface CommandeRepository extends CrudRepository<Commande,Long> {
-
-}
--- tp1/src/main/java/com/example/tp1/data/LigneRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tp1.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface LigneRepository extends CrudRepository<Ligne, Long> {
-}
