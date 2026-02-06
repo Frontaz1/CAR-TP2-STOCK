32391ebc6c2237b44448b19a3e8b5a4bbc063b32
Miroux Julien <julien.miroux.etu@b13p14.fil.univ-lille.fr>
2026-02-06 11:12:01 +0100

    feat: ajout kafka consume

---
+++ tp2/src/main/java/com/example/tp2/service/KafkaConsumer.java
@@ -0,0 +1,40 @@
+package com.example.tp2.service;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class KafkaConsumer {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+    private final StockService stockService;
+    private List<String> messages = Collections.synchronizedList(new ArrayList<>());
+
+    public KafkaConsumer(StockService stockService) {
+        this.stockService = stockService;
+    }
+
+    @KafkaListener(topics = "my-first-topic", groupId = "my-first-group")
+    public void consume(ConsumerRecord<String, String> record) {
+        LOGGER.info("Consumed message: {}", record.value());
+        String message = record.value();
+        String separateur= " ";
+        String[] myArray = message.split(separateur);
+        //j'aurais pu garder upsertStock et ajouter devant qte un "-"
+        String libelle = myArray[0];
+        int qte = Integer.parseInt(myArray[1]);
+        stockService.DeleteStock(libelle, qte);
+        messages.add(record.value());
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+}
